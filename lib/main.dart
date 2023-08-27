026d8b9f5c49b4009f27ce1d25671bcae18279c1
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-08-27 15:30:05 -0600

    create two main widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const DocumentApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DocumentApp extends StatelessWidget {
+  const DocumentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: DocumentScreen(
+        document: Document(),
+      ),
+    );
+  }
+}
+
+class DocumentScreen extends StatelessWidget {
+  final Document document;
+
+  const DocumentScreen({
+    required this.document,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title goes here'),
+      ),
+      body: const Column(
+        children: [
+          Center(
+            child: Text('Body goes here'),
+          ),
+        ],
       ),
     );
   }
