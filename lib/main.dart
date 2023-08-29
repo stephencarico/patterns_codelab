2b879a30bbb94b66be6417685a53f1fe620aa3e8
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-08-28 21:48:29 -0600

    BlockWidget

--- lib/main.dart
+++ lib/main.dart
@@ -48,3 +48,33 @@ class DocumentScreen extends StatelessWidget {
     );
   }
 }
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
+      ),
+    );
+  }
+}
